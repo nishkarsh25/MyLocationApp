eb231c7ad34d4e31d0b210ea3971d78cf6ab9ac0
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-03-01 04:19:41 +0530

    RememberLauncherForActivityResult - Starting the Permission request

--- app/src/main/java/eu/tutorials/mylocationapp/MainActivity.kt
+++ app/src/main/java/eu/tutorials/mylocationapp/MainActivity.kt
@@ -1,15 +1,26 @@
 package eu.tutorials.mylocationapp
 
+import android.Manifest
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import eu.tutorials.mylocationapp.ui.theme.MyLocationAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    LocationDisplay(locationUtils = LocationUtils(this),  context = this)
                 }
             }
         }
@@ -30,17 +41,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun LocationDisplay(
+    locationUtils: LocationUtils,
+    context: Context
+) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyLocationAppTheme {
-        Greeting("Android")
+
+
+    val requestPermissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions(),
+        onResult = {permissions->
+            if(permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true
+                && permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true){
+
+                // I have access to location
+            }
+            else{
+                // ask for permission
+            }
+
+        })
+
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+
+        Text(text = "Location not available")
+        Button(onClick = {
+            if (locationUtils.hasLocationPermission(context)) {
+                // Permission already granted update the location
+
+            } else {
+                // request location permission
+            }
+        }) {
+            Text(text = "Get Location")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        // You can add code to display location information here if needed
     }
 }
